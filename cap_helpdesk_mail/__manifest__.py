7ee0ff8dc09e40085466eea0629abad24f76cf17
Bechelot Nicolas <nicolas.bechelot@captivea.com>
2021-10-11 10:58:31 +0200

    Revert "Better manifest"
    This reverts commit c150e5da57023ffa0d216074c35e39f5354e0197.

--- cap_helpdesk_mail/__manifest__.py
+++ cap_helpdesk_mail/__manifest__.py
@@ -1,36 +1,24 @@
 # -*- coding: utf-8 -*-
--{
-    'name': "Captivea Helpdesk Mail",
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-    'summary': """
-        Remove Helpdesk\'s weird looking mail and his button""",
-
+{
+    'name': 'Captivea Helpdesk Mail',
+    'version': '1.0.1',
+    'author': "Captivea France",
+    'category': 'Services/CapHelpdeskMail',
+    'summary': 'Remove Helpdesk\'s weird looking mail and his button',
+    'depends': [
+        'helpdesk',
+    ],
     'description': """
 Captivea Helpdesk Mail Override
 =================
 Remove Helpdesk's weird looking mail and his button.
 
     """,
-
-    'author': "Captivea France",
-    'website': "https://www.captivea.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Services/CapHelpdeskMail',
-    'version': '1.0',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base', 'helpdesk'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'data/cap_mail_data.xml',
     ],
-
-    'installable': True,
-    'application': False,
     'auto_install': True,
+    'license': '',
 }
